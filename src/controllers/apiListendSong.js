7b0df1fb113addc361a38a437df7bde7c1413881
mvtvn78 <mvtvn78@gmail.com>
2024-06-13 15:11:58 +0700

    apiListenedSong fix bug 01

--- src/controllers/apiListendSong.js
+++ src/controllers/apiListendSong.js
@@ -77,13 +77,8 @@ const searchListendSong = async(req,res)=>{
     //get payload from body
     const maND = req.query.maND
     const maBH = req.query.maBH
-    let value = await User.getUserBy(["MAND"],[maND])
-    if(!value)
-        return res.json(ErrorServices("User code not found",-1,''))
-    value = await Song.getSongBy(["MABH"],[maBH])
-    if(!value)
-        return res.json(ErrorServices("Song code not found",-1,''))
-    value = await ListenedSong.searchListenedSong([getParamSearch(maND),getParamSearch(maBH)])
+  
+    const value = await ListenedSong.searchListenedSong([getParamSearch(maND),getParamSearch(maBH)])
     if(value)
         return res.json(ErrorServices("Retrive has succesful ",0,value))
    return res.json(ErrorServices("Retrive has failed",-1,''))
